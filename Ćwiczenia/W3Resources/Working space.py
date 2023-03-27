f89759075a48c9455cb7f0ad020df4947d023c5c
MateuszStrz <121893193+MatStrz@users.noreply.github.com>
2023-03-27 19:15:05 +0200

    adding exercises from w3 resources

---
+++ Ćwiczenia/W3Resources/Working space.py
@@ -0,0 +1,6 @@
+
+youtuber = "Mateusz"
+
+print("subscribe to:", youtuber)
+print("subscribe to: {}".format(youtuber))
+print(f"subscribe to: {youtuber}")
---
+++ Ćwiczenia/execises/test.py
@@ -0,0 +1,4 @@
+def wzorsktuconego(x, y):
+    return (x + y) * (x + y)
+
+print(wzorsktuconego(4,3))
